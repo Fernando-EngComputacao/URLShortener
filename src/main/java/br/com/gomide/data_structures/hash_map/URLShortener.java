9f8a705c75229a9caf678ff554c349ca7a78dbd7
RSantosIgor <s.igorrosa@gmail.com>
2022-10-31 23:27:27 -0300

    encodeBase64 and decodeBase64 finish

--- src/main/java/br/com/gomide/data_structures/hash_map/URLShortener.java
+++ src/main/java/br/com/gomide/data_structures/hash_map/URLShortener.java
@@ -1,23 +1,36 @@
 package br.com.gomide.data_structures.hash_map;
 
+import java.util.Base64;
+
 public class URLShortener implements IURLShortener {
 
 	@Override
 	public String encode(String url) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		return getPrefix() + encodeBase64(url);
 	}
 
 	@Override
 	public String decode(String encodedUrl) throws RuntimeException {
-		// TODO Auto-generated method stub
-		return null;
+		String code = getCode(encodedUrl);
+		return decodeBase64(code);
 	}
 
 	@Override
 	public String getPrefix() {
-		// TODO Auto-generated method stub
-		return null;
+		return "https://cut.com.br/";
 	}
-
+	
+	private String getCode(String url) {
+		return url.substring(getPrefix().length());
+	}
+	
+	private String decodeBase64(String seed) {
+		byte[] decodedBytes = Base64.getDecoder().decode(seed);
+		return new String(decodedBytes);
+	}
+	
+	private String encodeBase64(String seed) {
+		return Base64.getEncoder().encodeToString(seed.getBytes());
+	}
+	
 }
